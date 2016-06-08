c4aef88752bc20c1749eba3b29d0ac2b31f11913
Macey Baker <maceybaker@Maceys-MacBook-Pro-4.local>
2016-06-08 21:51:32 +0100

    Updated emoji sampler

--- emojify_rspec_formatter.gemspec
+++ emojify_rspec_formatter.gemspec
@@ -3,7 +3,7 @@ $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
 Gem::Specification.new do |spec|
   spec.name          = "EmojifyRspecFormatter"
-  spec.version       = '0.1.0'
+  spec.version       = '0.1.1'
   spec.authors       = ["Macey Baker"]
   spec.email         = ["macebake@gmail.com"]
 
--- lib/emojify_rspec_formatter.rb
+++ lib/emojify_rspec_formatter.rb
@@ -5,20 +5,20 @@ class EmojifyRspecFormatter < RSpec::Core::Formatters::BaseTextFormatter
 
   RSpec::Core::Formatters.register self, *[:example_passed, :example_failed]
 
-    def initialize(output)
-      @output = output
-    end
+  def initialize(output)
+    @output = output
+  end
 
-    def example_passed(passed)
-        @output.print (['🍺  ','🙌  ', '😍  ',
-                        '🆒  ','👌  ', '💘  '].sample).colorize(:background => :green)
-        puts passed.example.description.strip '\n'
-    end
+  def example_passed(passed)
+    puts passed.example.description.strip
+      @output.print (['🍺  ','🙌  ', '😍  ',
+                      '🆒  ','👌  ', '💘  '].sample).colorize(:background => :green)
+  end
 
-    def example_failed(failed)
-        puts failed.example.description.strip
-        @output.print (['😨  ', '😵  ', '🙅  ',
-                        '🆘  ', '👎  ', '💔  '].sample).colorize(:background => :red)
-    end
+  def example_failed(failed)
+    puts failed.example.description.strip
+    @output.print (['😨  ', '😵  ', '🙅  ',
+                    '🆘  ', '👎  ', '💔  '].sample).colorize(:background => :red)
+  end
 
 end
