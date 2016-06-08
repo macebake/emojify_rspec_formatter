9f1ca88f8771f4ba20ce775e0315178a6ee9b222
Macey Baker <maceybaker@Maceys-MacBook-Pro-4.local>
2016-06-08 21:27:05 +0100

    changed names

---
+++ lib/emojify_rspec_formatter.rb
@@ -0,0 +1,22 @@
+require 'rspec/core/formatters/base_text_formatter'
+require 'colorize'
+
+class EmojifyRspecFormat < RSpec::Core::Formatters::BaseTextFormatter
+
+  RSpec::Core::Formatters.register self, *[:example_passed, :example_failed]
+
+    def initialize(output)
+      @output = output
+    end
+
+    def example_passed(passed)
+        print passed.example.description
+        @output.print ([' 🍺 ',' 🙌 ', ' 😍 '].sample).colorize(:background => :green)
+    end
+
+    def example_failed(failed)
+        print failed.example.description
+        @output.print ([' 😨 ', ' 😵 ', ' 🙅 '].sample).colorize(:background => :red)
+    end
+
+end
